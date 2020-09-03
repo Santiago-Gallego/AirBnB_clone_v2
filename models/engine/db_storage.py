dda105cf25c9084d691c6fb071d04b618a876b45
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-09-03 00:37:07 +0000

    [Add]

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,37 +1,39 @@
 #!/usr/bin/python3
 """This module defines a class to manage db for hbnb AirBnb clone v2"""
-from sqlalchemy import create_engine
+import datetime
+from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
-from os import environ
-from models.state import State
-from models.city import City
+from sqlalchemy import create_engine
 from models.base_model import BaseModel, Base
 from models.user import User
-from models.place import Place
+from models.state import State
+from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
 
 
-class DBStorage:
-    """This class manages storage of hbnb models in Databases"""
+class DBStorage():
+    """
+    Database Engine for AirBnB project
+    """
     __engine = None
     __session = None
 
     def __init__(self):
-        user_db = environ.get('HBNB_MYSQL_USER')
-        pass_db = environ.get('HBNB_MYSQL_PWD')
-        host_db = environ.get('HBNB_MYSQL_HOST')
-        _db = environ.get('HBNB_MYSQL_DB')
-
-        self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}'
-            .format(user_db, pass_db, host_db, _db), pool_pre_ping=True)
-
-        if environ.get('HBNB_ENV') == 'test':
-            Base.metada.drop_all(bind=self.__engine)
+        """Init method"""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(bind=self.__engine)
 
     def all(self, cls=None):
-        """Return all objects"""
+        """Returns dictionary with all objects depending
+        of the class name (argument cls)"""
         if cls:
             objs = self.__session.query(self.classes()[cls])
         else:
@@ -49,25 +51,39 @@ class DBStorage:
         return dic
 
     def new(self, obj):
-        """add the object to the current database session """
+        """Add the object to the current
+        database session (self.__session)"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """Commit all changes of the current
+        database session (self.__session)"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
+        """Delete from the current database session obj if not None"""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create tables and current db session"""
+        """Create the current database session (self.__session) from
+        the engine (self.__engine) by using a sessionmaker"""
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
         Base.metadata.create_all(self.__engine)
-        session_fact = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_fact)
+        self.__session = sessionmaker(bind=self.__engine,
+                                      expire_on_commit=False)
+        Session = scoped_session(self.__session)
         self.__session = Session()
+
+    def close(self):
+        """Removes the session"""
+        self.__session.close()
 
     def classes(self):
         """Returns a dictionary of valid classes and their references."""
@@ -88,7 +104,40 @@ class DBStorage:
                    "Review": Review}
         return classes
 
-        def close(self):
-        """Removes the session"""
-        self.__session.close()
-        
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
